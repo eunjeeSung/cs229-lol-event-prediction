fff05d8b022f3b3f2d1741f7af242451d627117a
eunjeeSung <ismkaleidoscope@gmail.com>
2021-04-26 17:45:10 -0700

    Reword hidden_things ->  historic_acc_info

--- models/rmtpp/loss.py
+++ models/rmtpp/loss.py
@@ -2,16 +2,14 @@ import torch
 import torch.nn as nn
 
 
-def RMTPPLoss(hidden_things, time_duration):
+def RMTPPLoss(historic_acc_info, time_duration):
     """Adopted from https://github.com/Hongrui24/RMTPP-pytorch/blob/master/model.py#L23
     """
     intensity_w = nn.Parameter(torch.tensor(0.1, dtype=torch.float32))
-    intensity_b = nn.Parameter(torch.tensor(0.1, dtype=torch.float32))
 
     # print('/3: ', torch.mean(torch.exp(intensity_w * time_duration)))
-    loss = torch.mean(hidden_things \
-                + intensity_w * time_duration \
-                + intensity_b \
-                + (torch.exp(hidden_things + intensity_b) -
-                  torch.exp(hidden_things + intensity_w * time_duration + intensity_b)) / intensity_w)
+    loss = torch.mean(historic_acc_info \
+                + intensity_w * time_duration
+                + (torch.exp(historic_acc_info) -
+                  torch.exp(historic_acc_info + intensity_w * time_duration)) / intensity_w)
     return -loss
